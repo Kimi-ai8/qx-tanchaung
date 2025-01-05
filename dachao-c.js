16ca84bef1696d8e7838121daf688de1d8eaa30a
Kimi-ai8 <sorafatullamilon72@gmail.com>
2025-01-05 14:09:32 +0800

    Update dachao-c.js

--- dachao-c.js
+++ dachao-c.js
@@ -1,3 +1,10 @@
+/*************************************
+
+WX打开链接：https://m.aihoge.com/lottery/awardBonus/drawRedPacket?title=
+
+*************************************/
+
+
 [rewrite_local]
 https://m.aihoge.com/api/memberhy/h5/js/signature url script-request-header https://raw.githubusercontent.com/Kimi-ai8/qx-tanchaung/refs/heads/main/dachao.js
 
