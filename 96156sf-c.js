cbdf7307216617e67460add7eae8894a4240c21b
Kimi-ai8 <sorafatullamilon72@gmail.com>
2025-01-27 16:18:22 +0800

    Create 96156sf-c.js

---
+++ 96156sf-c.js
@@ -0,0 +1,12 @@
+/*************************************
+
+打开一下小程序96156获取x-token,和社服活动获取token。
+
+*************************************/
+[rewrite_local]
+https://ylapi.luckystarpay.com/api/getUserInfo url script-request-header https://raw.githubusercontent.com/Kimi-ai8/qx-tanchaung/refs/heads/main/96156sf.js
+
+https://sfapi.bjsfxh.com/api/getLevelRedPacket url script-request-header https://raw.githubusercontent.com/Kimi-ai8/qx-tanchaung/refs/heads/main/96156sf.js
+
+[mitm]
+hostname = ylapi.luckystarpay.com, sfapi.bjsfxh.com
