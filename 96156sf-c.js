c31a5a4439917eec4ac337c7687367625cd150f3
Kimi-ai8 <sorafatullamilon72@gmail.com>
2025-01-27 16:41:18 +0800

    Update 96156sf-c.js

--- 96156sf-c.js
+++ 96156sf-c.js
@@ -1,11 +1,8 @@
 /*************************************
-
-打开一下小程序社服活动获取x-token。
-
+打开一下小程序96156获取x-token,和社服活动获取token。
 *************************************/
 [rewrite_local]
-
+https://ylapi.luckystarpay.com/api/getUserInfo url script-request-header https://raw.githubusercontent.com/Kimi-ai8/qx-tanchaung/refs/heads/main/96156.js
 https://sfapi.bjsfxh.com/api/getLevelRedPacket url script-request-header https://raw.githubusercontent.com/Kimi-ai8/qx-tanchaung/refs/heads/main/96156sf.js
-
 [mitm]
-hostname = sfapi.bjsfxh.com
+hostname = ylapi.luckystarpay.com, sfapi.bjsfxh.com
