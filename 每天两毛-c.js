ccd5738d5bffca2edc1e81630733a74794212bc2
Kimi-ai8 <sorafatullamilon72@gmail.com>
2025-07-05 19:00:18 +0800

    Create 每天两毛-c.js

---
+++ 每天两毛-c.js
@@ -0,0 +1,5 @@
+[rewrite_local]
+https://whdy.igklh.cn/client/v3/course_info  url script-request-header https://raw.githubusercontent.com/Kimi-ai8/qx-tanchaung/refs/heads/main/每天两毛.js
+
+[mitm]
+hostname = whdy.igklh.cn
