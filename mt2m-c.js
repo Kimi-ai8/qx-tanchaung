76760a9600adaec63c463ee71cdb598f1e0b746a
Kimi-ai8 <sorafatullamilon72@gmail.com>
2025-07-05 19:20:28 +0800

    Update mt2m-c.js

--- mt2m-c.js
+++ mt2m-c.js
@@ -1,5 +1,5 @@
 [rewrite_local]
-https://whdy.igklh.cn/client/v3/submit_answer url script-request-header https://raw.githubusercontent.com/Kimi-ai8/qx-tanchaung/refs/heads/main/每天两毛.js
+https://whdy.igklh.cn/client/v3/submit_answer url script-request-header https://raw.githubusercontent.com/Kimi-ai8/qx-tanchaung/refs/heads/main/mt2m.js
 
 [mitm]
 hostname = whdy.igklh.cn
