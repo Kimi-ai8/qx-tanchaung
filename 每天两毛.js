f2eef15a742d09f9570bb9db395f2b91ebf6dce1
Kimi-ai8 <sorafatullamilon72@gmail.com>
2025-07-05 19:00:04 +0800

    Create 每天两毛.js

---
+++ 每天两毛.js
@@ -0,0 +1,17 @@
+/*************************************
+
+[rewrite_local]
+https://whdy.igklh.cn/client/v3/course_info  url script-request-header https://raw.githubusercontent.com/Kimi-ai8/qx-tanchaung/refs/heads/main/每天两毛.js
+
+[mitm]
+hostname = whdy.igklh.cn
+
+*************************************/
+let headers = $request.headers;
+let token= headers['token'];
+let Token= '${token}';
+
+console.log(Token);
+$notify("获取的Token, "", Token);
+
+$done();
